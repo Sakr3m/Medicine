b50f62c1667de93d51b546924826188e02e5ee46
Sakr3m <rising.lux.sakrem@gmail.com>
2026-08-02 15:11:35 +0200

    Badge Oggi non spinge più giu il contenuto

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "promemoria-v8";
+const CACHE = "promemoria-v9";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
