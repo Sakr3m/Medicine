a2f4c1c445db00be3c47e18bbf4e5b80aaef1b45
Sakr3m <rising.lux.sakrem@gmail.com>
2026-08-02 15:42:50 +0200

    Corretto lo sbilanciamento reale: tolto padding header, centraggio ora simmetrico

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "promemoria-v11";
+const CACHE = "promemoria-v12";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
