fb0ecd04afc1fb2a22e8f0b9c41125831349de30
Sakr3m <rising.lux.sakrem@gmail.com>
2026-08-02 14:19:25 +0200

    Scroll più rigido, gruppo antibiotico separato, testi più grandi, pallini più evidenti

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "promemoria-v2";
+const CACHE = "promemoria-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
