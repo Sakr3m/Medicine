8127c07b258b270c13b8b4f36091a59292c2f320
Sakr3m <rising.lux.sakrem@gmail.com>
2026-08-02 14:23:44 +0200

    Bump cache v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "promemoria-v3";
+const CACHE = "promemoria-v4";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
