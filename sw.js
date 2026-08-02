08f1d033cd0467eae730145238d3078f187e2bf4
Sakr3m <rising.lux.sakrem@gmail.com>
2026-08-02 14:30:10 +0200

    Scroll più leggero

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "promemoria-v5";
+const CACHE = "promemoria-v6";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
