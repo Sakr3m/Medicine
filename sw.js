472cb7bd8dbc2eb4c811ad2c63e8030ec788b9de
Sakr3m <rising.lux.sakrem@gmail.com>
2026-08-02 14:35:41 +0200

    Sostituisco scroll con navigazione a frecce

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "promemoria-v6";
+const CACHE = "promemoria-v7";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
