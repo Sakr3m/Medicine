b482fa46086c0a57879b0db9226d1eeb46742fce
Sakr3m <rising.lux.sakrem@gmail.com>
2026-08-02 14:26:32 +0200

    Ammorbidisco resistenza scroll laterale

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "promemoria-v4";
+const CACHE = "promemoria-v5";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
